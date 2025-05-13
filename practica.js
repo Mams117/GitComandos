23785a3383625a262935316830170d307e6ea9d8
Miranda Mario <adsoadso17@gmail.com>
2025-05-13 17:16:47 -0500

    haciendo pruebas con el commit individual

---
+++ practica.js
@@ -0,0 +1,13 @@
+let nombre = "Juan";
+let edad = 25;
+let ciudad = "Madrid";
+let esEstudiante = true;
+let hobbies = ["futbol", "musica", "cine"];
+let direccion = {
+  calle: "Gran Via",
+  numero: 10,
+  ciudad: "Madrid",
+};
+let fechaNacimiento = new Date("1998-05-15");
+let saludo = `Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`;
+console.log(saludo);
